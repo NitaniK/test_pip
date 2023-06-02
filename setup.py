d53b7a97e0915d9f06c4e4d5c58bf0a8ff52458f
NitaniK <e1c20074@oit.ac.jp>
2023-06-02 11:09:55 +0900

    add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(name="task_help",
+      author="nitani",
+      author_email="e1c20074@oit.ac.jp",
+      maintainer="nitani",
+      maintainer_email="e1c20074@oit.ac.jp",
+      description="help me",
+      long_description="aaa",
+      license="a",
+      url="https://github.com/NitaniK/test_pip.git",
+      version="0.0.1",
+      download_url="https://github.com/NitaniK/test_pip.git",
+      python_requires="",
+      install_requires="",
+      extras_require="",
+      packages="",
+      classifiers=""
+    )
